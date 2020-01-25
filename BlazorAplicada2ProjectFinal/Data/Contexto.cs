5d48b6af704a1178514bbd45214dfefbb54bd392
ezequiel huma brito <ezequielhuma@hotmail.com>
2020-01-24 20:27:06 -0400

    Creation of the class Contexto

---
+++ BlazorAplicada2ProjectFinal/Data/Contexto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlazorAplicada2ProjectFinal.Data
+{
+    public class Contexto : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder.UseSqlServer(@"Server=DESKTOP-B3FBQ4F;Database=RecursosHumanosWebDB;Trusted_Connection=true"));
+
+        }
+        public DbSet<Employees> Employees { get; set; }
+
+    }
+}
